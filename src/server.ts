e6389386700d6d493718ad67ad639c19a847910a
Prawee Wongsa <prawee@hotmail.com>
2025-09-16 18:06:22 +0700

    install @hono/node-server and script run

---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import { serve } from "@hono/node-server";
+import app from "./app";
+
+serve({ fetch: app.fetch, port: 3000 }, (info) => {
+    console.log(`Server is running at http://localhost:${info.port}`);
+});
