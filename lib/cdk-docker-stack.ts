0c66f4a7aa804b83ffb86d938820451b9a1e132b
Prawee Wongsa <prawee@hotmail.com>
2025-09-17 12:11:11 +0700

    update stack with docker

--- lib/cdk-docker-stack.ts
+++ lib/cdk-docker-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ecrAssets from 'aws-cdk-lib/aws-ecr-assets';
+import * as path from 'path';
 
 export class CdkDockerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn = new lambda.DockerImageFunction(this, `PocCdkDocker`, {
+      functionName: `poc-cdk-docker`,
+      description: 'POC Lambda with Docker',
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../'), {
+        platform: ecrAssets.Platform.LINUX_ARM64,
+        file: 'Dockerfile'
+      }),
+      architecture: lambda.Architecture.ARM_64,
+      memorySize: 256,
+      timeout: cdk.Duration.seconds(120),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDockerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new lambda.FunctionUrl(this, 'PocCdkDockerUrl', {
+      function: fn,
+      authType: lambda.FunctionUrlAuthType.NONE
+    });
+
+    new cdk.CfnOutput(this, 'PocCdkDockerEndpoint', {
+      value: api.url
+    });
   }
 }
